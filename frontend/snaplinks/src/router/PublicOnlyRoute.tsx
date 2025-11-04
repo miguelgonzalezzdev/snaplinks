a8d15e212a47243081c35dbf353dc60ce9fd47ff
miguelgonzalezzdev <miguelgonzalezdev@gmail.com>
2025-11-04 21:19:56 +0100

    Añadir funcionalidad iniciar sesion automaticamente si hay token

--- frontend/snaplinks/src/router/PublicOnlyRoute.tsx
+++ frontend/snaplinks/src/router/PublicOnlyRoute.tsx
@@ -1,8 +1,55 @@
 import { Navigate, Outlet } from "react-router"
 import { useAuthStore } from "../store/useAuthStore"
+import { Loader } from "../components/Loader"
+import { useEffect, useState } from "react"
 
 export const PublicOnlyRoute = () => {
   const isAuthenticated = useAuthStore((state) => state.isAuthenticated)
-
-  return isAuthenticated ? <Navigate to="/" replace /> : <Outlet />
+    const loadSession = useAuthStore((state) => state.loadSession)
+    const refreshTokenFn = useAuthStore((state) => state.refreshTokenFn)
+    const expiresAt = useAuthStore((state) => state.expiresAt)
+    const [authorized, setAuthorized] = useState(false)
+    const [loading, setLoading] = useState(true)
+  
+    useEffect(() => {
+      const checkAuth = async () => {
+  
+        const now = Date.now()
+  
+        // Si ya está autenticado y token no expirado
+        if (isAuthenticated && expiresAt && now < expiresAt) {
+          setAuthorized(true)
+          setLoading(false)
+          return
+        }
+  
+        // Intentamos cargar sesion previa
+        const session = loadSession()
+        if (session.success) {
+  
+          // Si la sesion es valida
+          if (session.isValid) {
+            setAuthorized(true)
+            setLoading(false)
+            return
+          }
+  
+          // Refrescamos token
+          const refreshResult = await refreshTokenFn()
+          setAuthorized(refreshResult.success)
+          setLoading(false)
+          return
+        }
+  
+        // Si todo falla, no autorizado
+        setAuthorized(false)
+        setLoading(false)
+      }
+  
+      checkAuth()
+    }, [isAuthenticated, loadSession, refreshTokenFn, expiresAt])
+  
+    if (loading) return <Loader />
+  
+    return authorized ? <Navigate to="/dashboard" replace /> : <Outlet />
 }
