d199be9c7cea825494f83dd4b61e58ae53011686
miguelgonzalezzdev <miguelgonzalezdev@gmail.com>
2025-11-05 17:26:22 +0100

    Añadir funcionalidad logout

--- frontend/snaplinks/src/components/Header.tsx
+++ frontend/snaplinks/src/components/Header.tsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router";
+import { useAuthStore } from "../store/useAuthStore";
 
 export default function Header() {
+    const logoutStore = useAuthStore((state) => state.logout)
+
+    const handleLogout = () => {
+        logoutStore()
+    }
+
     return (
         <header className="border-b border-gray-800 bg-gray-900/70 backdrop-blur-lg sticky top-0 z-50">
-            <div className="max-w-7xl mx-auto px-8 py-4 flex items-center justify-between">
+            <div className="mx-auto px-4 md:px-14 py-4 flex items-center justify-between">
                 <Link to="/dashboard" className="flex items-center gap-3">
                     <img
                         src="/snaplinks-logo.png"
@@ -13,16 +20,16 @@ export default function Header() {
                     <h2 className="text-xl sm:text-2xl font-semibold text-gray-100 hidden sm:block">SnapLinks</h2>
                 </Link>
 
-                <nav className="flex items-center gap-6 text-md">
-                    <Link to="/dashboard" className="hover:text-indigo-400 transition-colors">
+                <nav className="flex items-center gap-4 text-md md:text-lg">
+                    <Link to="/dashboard" className="hover:text-indigo-400 transition-colors p-2 rounded-md hover:bg-indigo-500/10">
                         Inicio
                     </Link>
-                    <Link to="/" className="hover:text-indigo-400 transition-colors">
+                    <Link to="/" className="hover:text-indigo-400 transition-colors p-2 rounded-md hover:bg-indigo-500/10">
                         Estadísticas
                     </Link>
-                    <Link to="/" className="hover:text-indigo-400 transition-colors">
+                    <p onClick={handleLogout} className="hover:text-indigo-400 transition-colors cursor-pointer p-2 rounded-md hover:bg-indigo-500/10">
                         Salir
-                    </Link>
+                    </p>
                 </nav>
             </div>
         </header>
